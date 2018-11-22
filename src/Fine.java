d704303783e6ef741d618df2eab8305e1849d303
Scott Simmons <scottsimmons345@gmail.com>
2018-11-22 17:46:38 +0000

    Comment and Code order meddling
    Reordered code and completed comments to meet Liam's standards. Needs reviewing by someone else

--- src/Fine.java
+++ src/Fine.java
@@ -3,6 +3,7 @@ import java.util.Calendar;
 /**
  * This class models and handles a fine attached to a user's loan of a resource.
  * @author Scott (960689)
+ * NEEDS REVIEWING PLEASE
  *
  */
 public class Fine {
@@ -18,37 +19,8 @@ public class Fine {
 	private int lastDay;
 	
 	/**
-	 * Takes in ID of the history to attach to and the resource's fixed fine
-	 * @param historyID ID of the history
-	 * @param fixedFine Fixed fine of the resource
-	 */
-	public Fine(int historyID, int fixedFine) {
-		this.historyID = historyID;
-		this.fixedFine = fixedFine;
-		lastDay = Calendar.DAY_OF_YEAR;
-	}
-
-	/**
-	 * Reduces fine by a specified value 'amount'
-	 * @param amount The amount to pay off
-	 */
-	public void payFine(int amount) {
-		totalFine -= amount;
-		updateFine();
-		lastDay = Calendar.DAY_OF_YEAR;
-	}
-	
-	/**
-	 * Updates the total fine accumulated and overdue days
-	 */
-	public void updateFine() {
-		int daysPassed = calcDays(lastDay);
-		totalFine += fixedFine * daysPassed;
-		overdueDays += daysPassed;
-	}
-	
-	/**
-	 * Calculates difference between last day recorded and current day
+	 * Calculates the difference between the last day recorded and 
+	 * the current day
 	 * @param lastDay The last day recorded
 	 * @return Difference between current and last recorded day
 	 */
@@ -76,7 +48,7 @@ public class Fine {
 	}
 	
 	/**
-	 * Determines if year is a leap year
+	 * Determines if the input year is a leap year
 	 * @param year Year to check
 	 * @return True for leap year, false otherwise
 	 */
@@ -86,6 +58,36 @@ public class Fine {
 		}
 		
 		return false;
+	}
+	
+	/**
+	 * Takes in ID of the history to attach to and the resource's fixed fine
+	 * @param historyID ID of the history
+	 * @param fixedFine Fixed fine of the resource
+	 */
+	public Fine(int historyID, int fixedFine) {
+		this.historyID = historyID;
+		this.fixedFine = fixedFine;
+		lastDay = Calendar.DAY_OF_YEAR;
+	}
+
+	/**
+	 * Reduces the fine by a specified value 'amount'
+	 * @param amount The amount to pay off
+	 */
+	public void payFine(int amount) {
+		totalFine -= amount;
+		updateFine();
+		lastDay = Calendar.DAY_OF_YEAR;
+	}
+	
+	/**
+	 * Updates the total fine accumulated and overdue days
+	 */
+	public void updateFine() {
+		int daysPassed = calcDays(lastDay);
+		totalFine += fixedFine * daysPassed;
+		overdueDays += daysPassed;
 	}
 	
 	/**
