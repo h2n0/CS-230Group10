f943bcc131cad28c0770749035bc831d039bda2a
Jack Cullop <jack.cullop@hotmail.co.uk>
2018-11-25 19:05:04 +0000

    Updated login page to login

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,6 +1,54 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**This controllers the ui for the login page
+ * 
+ * @author Jack
+ *
+ */
 public class LoginController {
 
+	@FXML
+	private Label userNotFound;
 	
+	@FXML
+	private TextField usernameField;
+	
+	@FXML
+	private Button loginButton;
+	
+	@FXML
+	private void handleLoginAction(ActionEvent event ) {
+		//DatabaseManager.searchpartial()
+		String inputUsername = usernameField.getText();
+		if(inputUsername == "test")
+		{
+			changeToMainPage();
+		}
+	}
+	
+	private void changeToMainPage() {
+		BorderPane root = null;
+		try {
+			root = (BorderPane)FXMLLoader.load(getClass().getClassLoader().getResource("MainPage.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(root,400,400);
+		scene.getStylesheets().add(getClass().getClassLoader().getResource("application.css").toExternalForm());
+		Stage stage = (Stage)loginButton.getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getClassLoader().getResource("MainPage.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getClassLoader().getResource("Login.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getClassLoader().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
