4c3419afd1a401266904dd0445a75693052aa037
Oliver Lee <ollie.lee@icloud.com>
2018-11-26 12:50:39 +0000

    UpdateLaptop.java

--- Laptop.java
+++ Laptop.java
@@ -30,25 +30,12 @@ package Resources;
  		this.manufacturer=manufacturer;
  		this.model=model;
  		this.operatingSystem=operatingSystem;
+ 		this.type = "Laptop";
+// 		create();
 		
 	}
-	/**
-	 * The constructor of a Laptop that already exists in the Database
-	 * @param id The id of the Resource
-	 */
-	// public Laptop (int id) {
-		// super(id);
-		// String sId = Integer.toString(id);
-		// String[][] Data =new String[1][5];
-		// Data = DatabaseManager.Search("Laptop",sId);
-		// String manufacturer = Data [0][1];
-		// String model = Data [0][1];
- 		// String operatingSystem = operatingSystem [0][1];
-		// this.manufacturer=manufacturer;
-		// this.model=model;
- 		// this.operatingSystem=operatingSystem;
-	
-	// }
+ 	
+ 	
  	
  	/**
 	 * Sets the manufacturer
@@ -56,7 +43,6 @@ package Resources;
  	 */
  	public  void setManufacturer (String manufcaturer) {
  		this.manufacturer=manufacturer;
- 		this.update("Manufacturer",manufacturer);
  	}
  	/**
  	 */
@@ -67,7 +53,6 @@ package Resources;
  	 */
  	public  void setModel (String model) {
  		this.model=model;
- 		this.update("Model",model);
  	}
  	/**
  	 */
@@ -78,7 +63,6 @@ package Resources;
  	 */
  	public  void setOperatingSystem (String operatingSysytem) {
  		this.operatingSystem=operatingSystem;
- 		this.update("Operating System",operatingSystem);
  	}
  	
  	
@@ -109,9 +93,8 @@ package Resources;
  	 * @param attribute The new value of the attribute
  	 */
  	@Override
- 	public void update (String field , String attribute) {
- 		String sId = Integer.toString(this.id);
- //		DatabaseManager.Update("Laptop", field,sId,attribute);
+ 	public void update () {
+
  	}
  	
  	
@@ -120,20 +103,7 @@ package Resources;
 	 */
 	public void create () {
 		
-		String sId = Integer.toString(this.id);
-		String sYear = Integer.toString(this.year);
-		String sNumCopies = Integer.toString(this.numCopies);
-		String sAvailableCopies = Integer.toString(this.availableCopiesNum);
-		String laptopData[] = {this.manufacturer,this.model,this.operatingSystem};
-		String resourceData[] = {sId,this.title,sYear,this.thumbnail,sNumCopies,sAvailableCopies};
-//		DatabaseManager.Create("Laptop",laptopData);
-//		DatabaseManager.Create("Resource",resourceData);
-		Queue<Integer> queue = this.requestQ;
-		while (!queue.isEmpty()) {
-			String queueData[] =  {sId,queue.peek().toString()};
-//			DatabaseManager.Create("Queue",queueData);
-			queue.remove();
-		}
+		DatabaseManager.saveRecord(this,"Laptop");
 	}
  	
 	/**
@@ -141,10 +111,7 @@ package Resources;
 	 */
 	public void delete () {
 		
-		String sId = Integer.toString(this.id);
-//		DatabaseManager.Delete("Resource",sId);
-//		DatabaseManager.Delete("Laptop",sId);
-//		DatabaseManager.Delete("Queue",sId);
+		DatabaseManager.deleteRecord(this,"Laptop");
 	}
  	
 	
