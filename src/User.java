d3a7534bbcb542aa05be29b325b04b6f5495ecc3
Elliot Lee-Cerrino <elc954@hotmail.co.uk>
2018-11-26 14:46:27 +0000

    Fixed my branch

--- src/User.java
+++ src/User.java
@@ -1,66 +1,7 @@
-
-/**
-<<<<<<< HEAD
- * The class handles all of the user functions and responsibilities
- * @author Elliot Lee-Cerrino (964552)
- *
- */
-public class User {
-
-	// User properties
-	private String userID;
-	private String name;
-	private String address;
-	private int balance;
-	private String avatarPath;
-	
-	/**
-	 * Recreate a user based on information in the database
-	 * @param id - The User's ID in the data base
-	 */
-	public User(int id) {
-		// Unused at the moment, waiting for database connections
-	}
-	
-	/**
-	 * Create a brand new user from some basic info
-	 * @param name - The users name
-	 * @param address - The address at which they live at
-	 */
-	public User(String name, String address) {
-		this.name = name;
-		this.address = address;
-	}
-	
-	/**
-	 * Allows the user to draw a custom avatar for their profile
-	 */
-	public void drawAvatar() {
-		
-	}
-	
-	/**
-	 * Allows a user to select from pre-made or custom avatars for their profile 
-	 */
-	public void pickAvatar() {
-		
-	}
-	
-	/**
-	 * Check to see if the provided address is a valid one
-	 * 
-	 * @param newAddress - The address to check
-	 * @return boolean - True if address is valid and False otherwise
-	 */
-	private boolean validateAddress(String newAddress) {
-		return false;
-	}
-}
-=======
- * User stores the info for each user
+/** User stores the info for each user
  * @author 963257
  * @version 1.0
- */
+ **/
 
 public class User {
 	//Uniquely identify each user
@@ -173,4 +114,3 @@ public class User {
 	}
 	
 }
->>>>>>> de9a5512ad6d063a9537fbdd27769443317b452c
