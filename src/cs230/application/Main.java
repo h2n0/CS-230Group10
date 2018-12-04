09fce0c9fa9893005197f1a66f4183f887785c89
kaleb <44204898+kaleb99@users.noreply.github.com>
2018-12-04 23:47:49 +0000

    added schedule to update database

--- src/cs230/application/Main.java
+++ src/cs230/application/Main.java
@@ -1,5 +1,13 @@
 package cs230.application;
 	
+import java.util.Date;
+
+import java.util.Timer;
+
+import cs230.system.Copy;
+import cs230.system.DatabaseManager;
+import cs230.system.User;
+import cs230.system.UpdateDatabase;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -12,6 +20,21 @@ import javafx.stage.StageStyle;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		try {
+			//get todays date
+			Date today = new Date();
+			
+			//long delay is time in milliseconds between the scheduled code 
+			long delay = 10000;
+			
+			//set a new timer and load in updateDB and set to run everyday at midday
+			Timer timer = new Timer();
+			timer.schedule(new UpdateDatabase(), today, delay );
+			
+		} catch (Exception e){
+			System.out.println("database update failed");
+		}
+		
 		try {
 			primaryStage.initStyle(StageStyle.UNDECORATED);
 			VBox root = (VBox)FXMLLoader.load(getClass().getClassLoader().getResource("cs230/application/Fine.fxml"));
@@ -27,4 +50,5 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
