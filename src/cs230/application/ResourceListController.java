e5bc9b3ade9a76d715f1e052394a3ee8f1cc0d8a
kaleb <44204898+kaleb99@users.noreply.github.com>
2018-12-07 00:05:00 +0000

    bug fixes copyhistory

--- src/cs230/application/ResourceListController.java
+++ src/cs230/application/ResourceListController.java
@@ -85,11 +85,12 @@ public class ResourceListController  {
 			e.printStackTrace();
 		}
 		
+		//how the shit do i make a list of resources without instantiating resources
 		ArrayList<Resource> allResources = (ArrayList<Resource>) allBooks;
 		allResources.append((ArrayList<Resource>) allDVDs);
 		allResources.append((ArrayList<Resource>) allLaptops);
 		
-		//populate the table with the users above
+		//populate the table with the resources above
 		PopulateFineTable(allResources);
     }
     
