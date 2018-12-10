513214c4d0b173d08b6b42435c16533daeefd51d
Jack Cullop <jack.cullop@hotmail.co.uk>
2018-12-10 07:05:12 +0000

    try this

--- src/cs230/application/Main.java
+++ src/cs230/application/Main.java
@@ -1,9 +1,15 @@
 package cs230.application;
 
+import java.util.ArrayList;
+
+import cs230.system.DatabaseManager;
+import cs230.system.SharedData;
+import cs230.system.User;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -14,6 +20,23 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		//try setting up the scheduler for the database update
 		//launch the login window to start the program
+	        ArrayList<User> userList = (ArrayList<User>) DatabaseManager.getTable("user");
+	        if(userList == null || userList.isEmpty())
+	        {
+	                SharedData.setIsFirstRun(true);
+	                try {
+	                        VBox root =
+	                        FXMLLoader.load(getClass().getClassLoader().getResource("cs230/application/NewUser.fxml"));
+	                    primaryStage.initStyle(StageStyle.UNDECORATED);
+	                    Scene scene = new Scene(root);
+	                    primaryStage.setScene(scene);
+	                    primaryStage.centerOnScreen();
+	                    primaryStage.show();
+	                    primaryStage.centerOnScreen();
+	                } catch(Exception e) {
+	                    e.printStackTrace();
+	                } 
+	        }
 		try {
 		        AnchorPane root =
 				FXMLLoader.load(getClass().getClassLoader().getResource("cs230/application/Login.fxml"));
